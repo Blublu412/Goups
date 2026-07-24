5f7ef47270f7a789de6fa741ef9708c3217b486f
Bleuenn Guilleux <bleuenn.guilleux@etudiant.univ-rennes.fr>
2026-07-24 17:47:11 +0200

    Feat: formulaire de reation de produit et ajout à la db et au panier

--- app/add_product_form.tsx
+++ app/add_product_form.tsx
@@ -1,26 +1,50 @@
 import { Text, View, TextInput, Button, Alert } from "react-native"
 import { useForm, Controller } from "react-hook-form"
+import{ addproductToDB, addProductToCart} from '../src/services/scan'
 
 interface AddProductProps {
     codeEAN: number;
+    cart_id: number;
 }
 
 interface FormInputs {
     EAN: string;
     Produit: string;
     prix: string;
+    type: string;
+    description: string
 //ajouter au fur et à mesure de l'avancement du formulaire
 }
 
-export default function addProduct({codeEAN} : AddProductProps) {
+export default function addProduct({codeEAN, cart_id} : AddProductProps) {
     const{ control, handleSubmit, formState: {errors}} = useForm({defaultValues:{
         EAN : String(codeEAN),
         Produit : "",
         prix: "",
+        type : "",
+        description: "",
 
     }})
 
-    const onSubmit = async (data: {})=>{}// à changer et mettre la fonction addProductToDB et remplir avec les champs du formulaire
+    const onSubmit = async (data: FormInputs)=>{
+        try{
+            const eanAsNumber = parseInt(data.EAN, 10);
+            const priceAsFloat = parseFloat(data.prix);
+
+            const newId = await addproductToDB({
+                EAN: eanAsNumber,
+                name: data.Produit,
+                price: priceAsFloat,
+                type: data.type,
+                decription: data.description
+            });
+
+            await addProductToCart(newId, cart_id);
+            //alert("Produit ajouté au panier");
+        }catch{
+            alert("Erreur lors de l'insertion en base de données.");
+        }
+    }// à changer et mettre la fonction addProductToDB et remplir avec les champs du formulaire
 
     return(
         <View>
@@ -73,7 +97,39 @@ export default function addProduct({codeEAN} : AddProductProps) {
                     </View>
                 )}/>
             {errors.prix && <Text>Le prix est requis</Text>}
-            <Button title="Submit" onPress={handleSubmit(onSubmit)} />
+            <Controller
+                control={control}
+                rules={{
+                    required:true,
+                }}
+                name="type"
+                render={({field : {onChange, onBlur, value}})=>(
+                    <View>
+                        <Text>Type du produit:</Text>
+                        <TextInput
+                        placeholder="mettre le type du produit"
+                        onChange={onChange}
+                        value={value}/>
+                    </View>
+                )}/>
+                {errors.type && <Text>Le type est requis</Text>}
+                <Controller
+                control={control}
+                rules={{
+                }}
+                name="description"
+                render={({field : {onChange, onBlur, value}})=>(
+                    <View>
+                        <Text>Description:</Text>
+                        <TextInput
+                        multiline
+                        numberOfLines={4}
+                        onChange={onChange}
+                        value={value}/>
+                    </View>
+                )}/>
+                {errors.type && <Text>Le type est requis</Text>}
+            <Button title="Ajouter au panier" onPress={handleSubmit(onSubmit)} />
         </View>
     )
 
