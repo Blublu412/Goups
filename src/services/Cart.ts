f37cbb771df84bc603d290e7281b368ea23ab6fc
Bleuenn Guilleux <bleuennbonbonfraise@gmail.com>
2026-08-23 16:29:41 -0400

    Feat: ajout de méthode pour le cart

--- src/services/Cart.ts
+++ src/services/Cart.ts
@@ -2,6 +2,11 @@ import { supabase } from '../lib/supabase';
 import { Tables} from '../lib/database.types';
 
 type Cart = Tables<'Cart'>;
+type Product = Tables<'Product'>;
+
+interface productId{
+    id_product: number; 
+}
 
 /**
  * 
@@ -19,3 +24,39 @@ export const createCart = async(user_id: number) : Promise<number>=>{
     }
     return data.id;
 };
+
+/**
+ * 
+ */
+export const getCarts = async(user_id: number): Promise<Cart[]> =>{
+    const date = new Date();
+    date.setDate(date.getDate()-31);
+    const {data, error} = await supabase
+    .from('Cart')
+    .select()
+    .lt('created_at', date);
+
+    if(error){
+        console.error("Erreur lors de la récupération :", error.message);
+        throw error;
+    }
+    return data
+}
+
+/**
+ * 
+ */
+export const getProductIdInCart = async(cart_id:number): Promise<productId[]> =>{
+    const {data, error} = await supabase
+    .from('Contain')
+    .select('id_product')
+    .eq('id_cart',cart_id)
+
+    if(error){
+        console.error("Erreur lors de la récupération :", error.message);
+        throw error;
+    }
+    
+    return data;
+
+}
